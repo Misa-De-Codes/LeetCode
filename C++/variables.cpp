0ad67b9a27509004078e0d7f97eeace539a914c2
Misa-De-Codes <musaddikulislam007@gmail.com>
2025-06-04 08:20:38 +0530

    first commit

---
+++ C++/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+    // integer
+    int days = 100;
+    int age = 19;
+    int year = 2020;
+
+    //double (number including decimal)
+    double price = 34.43;
+    double gpa = 9.7;
+
+    // single character
+    char grade = 'A';
+    char currency = '$';
+
+    // boolean
+    bool student = true;
+    bool power = false;
+
+    // strings
+    std::string name = "Musa ";
+
+    std::cout << "Hellow " << name << age << grade << " " << days << " " << power << " " << student;
+}
