9118f49fb89eaab9860548fad31f5662a1460ae9
Misa-De-Codes <musaddikulislam007@gmail.com>
2025-06-11 00:48:44 +0530

    one more im done

--- DSA/Array/left_rotating.cpp
+++ DSA/Array/left_rotating.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 // single rotate
@@ -10,22 +10,50 @@ void rotate(int arr[], int n)
     {
         arr[i] = arr[i + 1];
     }
-    arr[n-1] = temp;
+    arr[n - 1] = temp;
 
     for (int j = 0; j < n; j++)
     {
         cout << arr[j] << " ";
     }
 }
-//multiple rotate
-void rotateByPlaces(int arr[], int n){
+
+// multiple rotate
+void rotateByPlaces1(int arr[], int n, int placeNo)
+{
+    int place = placeNo % n;
+    int temp[place];                // space O(space)
+    for (int i = 0; i < place; i++) // time O(place)
+    {
+        temp[i] = arr[i];
+    }
+
+    for (int i = place; i < n; i++) // time O(n - place)
+    {
+        arr[i - place] = arr[i];
+    }
+    for (int i = n - place; i < n; i++) // time O(place)
+    {
+        arr[i] = temp[i - (n - place)];
+        // i is increasing by 1 in each time
+        // loop runs but the n and places are constant so they r not gonna
+        // catch up with that +1 in i
+    }
+    /*
+        time = O(place) + O(n - place) + O(place) = O(n + place)
+        space = O(space) 
+    */
+
     for (int i = 0; i < n; i++)
     {
-        
+        cout << arr[i] << " ";
     }
-    
 }
 
+void rotateByPlaces2(int arr[], int n, int placeNo)
+{
+    
+}
 
 
 
@@ -33,7 +61,7 @@ int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    rotate(arr, 9);
+    rotateByPlaces2(arr, 9, 4);
 
     return 0;
 }
